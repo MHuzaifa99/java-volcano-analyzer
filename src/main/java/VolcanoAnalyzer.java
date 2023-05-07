c6b5eea8bb7ec872376791c10267c774f4e6d569
Muhammad Huzaifa <muhammadhuzaifa457@gmail.com>
2023-05-08 03:29:24 +0500

    first test case pass

--- src/main/java/VolcanoAnalyzer.java
+++ src/main/java/VolcanoAnalyzer.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -13,22 +15,40 @@ public class VolcanoAnalyzer {
     private List<Volcano> volcanos;
 
     public void loadVolcanoes(Optional<String> pathOpt) throws IOException, URISyntaxException {
-        try{
+        try {
             String path = pathOpt.orElse("volcano.json");
             URL url = this.getClass().getClassLoader().getResource(path);
             String jsonString = new String(Files.readAllBytes(Paths.get(url.toURI())));
             ObjectMapper objectMapper = new ObjectMapper();
             TypeFactory typeFactory = objectMapper.getTypeFactory();
-            volcanos = objectMapper.readValue(jsonString, typeFactory.constructCollectionType(List.class, Volcano.class));
-        } catch(Exception e){
-            throw(e);
+            volcanos = objectMapper.readValue(jsonString,
+                    typeFactory.constructCollectionType(List.class, Volcano.class));
+        } catch (Exception e) {
+            throw (e);
         }
     }
 
-    public Integer numbVolcanoes(){
+    public Integer numbVolcanoes() {
+        System.out.println(volcanos.size());
         return volcanos.size();
     }
 
-    //add methods here to meet the requirements in README.md
+    public List<Volcano> eruptedInEighties() {
+        List<Volcano> data = volcanos.stream().filter(n -> {
+            int a = n.getYear();
+            return a > 1979 && a < 1990;
+        }).collect(Collectors.toList());
+        System.out.println(data);
+        return data;
+    }
+
+    // add methods here to meet the requirements in README.md
+
+    public static void main(String args[]) throws IOException, URISyntaxException {
+        VolcanoAnalyzer a = new VolcanoAnalyzer();
+        // a.loadVolcanoes(Optional.empty());
+        a.eruptedInEighties();
+        // a.numbVolcanoes();
+    }
 
 }
