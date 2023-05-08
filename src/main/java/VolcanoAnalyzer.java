3147d57e124e8a93db95e5dba2dc85aacadc4a11
Muhammad Huzaifa <muhammadhuzaifa457@gmail.com>
2023-05-09 01:18:52 +0500

    all test cases passed

--- src/main/java/VolcanoAnalyzer.java
+++ src/main/java/VolcanoAnalyzer.java
@@ -3,16 +3,12 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.xml.validation.Validator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -48,61 +44,92 @@ public class VolcanoAnalyzer {
         return data;
     }
 
-    public String[] highVEI(){
-        return volcanos.stream().filter(n -> n.getVEI() >= 6).map(n -> n.getName()).collect(Collectors.toList()).toArray(String[]::new);
+    public String[] highVEI() {
+        return volcanos.stream().filter(n -> n.getVEI() >= 6).map(n -> n.getName())// .collect(Collectors.toList())
+                .toArray(String[]::new);
         // return data;
     }
 
-    public Volcano mostDeadly(){
+    public Volcano mostDeadly() {
         Optional<Volcano> data = volcanos.stream()
-        .filter(n -> !n.getDEATHS().isEmpty())
-        .max(Comparator.comparingInt(a -> Integer.parseInt(a.getDEATHS())));
+                .filter(n -> !n.getDEATHS().isEmpty())
+                .max(Comparator.comparingInt(a -> Integer.parseInt(a.getDEATHS())));
         return data.orElse(null);
     }
 
-    public double causedTsunami(){
-        return volcanos.stream().filter(v -> !v.getTsu().isEmpty()).count() *100 / volcanos.size();
+    public double causedTsunami() {
+        return volcanos.stream().filter(v -> !v.getTsu().isEmpty()).count() * 100 / volcanos.size();
     }
 
-    public String mostCommonType(){
+    public String mostCommonType() {
         // HashMap <String,Integer> str = new HashMap<String, Integer>();
         // Stream<Volcano> data = volcanos.stream();
         // str.put(data.getType(), 1);
         // data.forEach(n -> n.getType());
         // return "";
-        return  volcanos.stream()
-        .collect(Collectors.groupingBy(Volcano::getType, Collectors.counting()))
-        .entrySet().stream()
-        .max(Map.Entry.comparingByValue())
-        .map(Map.Entry::getKey)
-        .orElse(null);
+        return volcanos.stream()
+                .collect(Collectors.groupingBy(Volcano::getType, Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
 
     }
 
-    public int eruptionsByCountry(String country){
-        
+    public int eruptionsByCountry(String country) {
+
         return (int) volcanos.stream()
-        .filter(n -> n.getCountry().equalsIgnoreCase(country)).count();
+                .filter(n -> n.getCountry().equalsIgnoreCase(country)).count();
     }
 
-    public double averageElevation(){
-       return volcanos.stream().mapToDouble( Volcano :: getElevation).average().orElse(0);
+    public double averageElevation() {
+        return volcanos.stream().mapToDouble(Volcano::getElevation).average().orElse(0);
     }
 
-    public String[] volcanoTypes(){
-        return volcanos.stream().map(Volcano :: getType).distinct().toArray(String[] :: new);
+    public String[] volcanoTypes() {
+        return volcanos.stream().map(Volcano::getType).distinct().toArray(String[]::new);
     }
     // add methods here to meet the requirements in README.md
 
     public double percentNorth() {
-       double value =  volcanos.stream().filter(n -> n.getLatitude() > 0 ).count();
-        return value/volcanos.size()*100;
+        double value = volcanos.stream().filter(n -> n.getLatitude() > 0).count();
+        return value / volcanos.size() * 100;
     }
 
-    // public static void main(String args[]) throws IOException, URISyntaxException {
-    //     VolcanoAnalyzer a = new VolcanoAnalyzer();
-    //     // a.loadVolcanoes(Optional.empty());
-    //     a.eruptedInEighties();
-    //     // a.numbVolcanoes();
+    public String[] manyFilters() {
+        return volcanos.stream()
+                .filter(n -> n.getYear() > 1800).filter(n -> n.getTsu().isEmpty()).filter(n -> n.getLatitude() < 0)
+                .filter(n -> n.getVEI() == 5).map(Volcano::getName).toArray(String[]::new);
+    }
+
+    public String[] elevatedVolcanoes(int ele) {
+        return volcanos.stream().filter(n -> n.getElevation() >= ele).map(Volcano::getName).toArray(String[]::new);
+    }
+
+    public String[] topAgentsOfDeath() {
+        return volcanos.stream().filter(v -> !v.getDEATHS().isEmpty())
+                .sorted((i, j) -> Integer.parseInt(j.getDEATHS()) - Integer.parseInt(i.getDEATHS()))
+                .limit(10).filter(v -> !v.getAgent().isEmpty())
+                .map(v -> Arrays.asList(v.getAgent().split(","))).flatMap(List::stream)
+                .distinct()
+                .collect(Collectors.toList())
+                .toArray(new String[0]);
+    }
+
+    // public String[] topAgentsOfDeath(){
+    // // return
+    // String data = volcanos.stream().filter(n -> !n.getDEATHS().isEmpty())
+    // .max(Comparator.comparingInt(a -> Integer.parseInt(a.getDEATHS())))
+    // .map(Volcano::getAgent).orElse(null);
+    // // .toArray(String[]::new);
+
+    // // toArray(String[]::new);
+    // }
+    // public static void main(String args[]) throws IOException, URISyntaxException
+    // {
+    // VolcanoAnalyzer a = new VolcanoAnalyzer();
+    // // a.loadVolcanoes(Optional.empty());
+    // a.eruptedInEighties();
+    // // a.numbVolcanoes();
     // }
 }
